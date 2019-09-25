b2ff992299f78c3d9c50a1dcaba6066c3b2d2213
tian0 <sebas.aspe.von@gmail.com>
2019-09-25 14:13:05 -0700

    changes to app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola ' + req.query.nombre + '!');
+if (req.query.nombre == null) {
+    return res.send('<h1>Hola desconocido!</h1>');
+}
+
+else {
+  return res.send('<h1>Hola ' + req.query.nombre + '!</h1>');
+}
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
